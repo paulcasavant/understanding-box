f7f4f0449fe9741e27df6bb4fbb08f6e600a377d
Paul Casavant <thepaulcasavant@gmail.com>
2021-01-30 22:21:35 -0800

    latest

---
+++ templates/other.html
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>Other page</h1>
+</body>
+</html>
--- web.py
+++ web.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
 
 @app.route('/cakes')
 def cakes():
-    return 'Yummy cakes!'
+    return render_template('other.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+

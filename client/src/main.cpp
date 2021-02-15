74153e8ba121bc9497307ea9cc1dcd4f3b3ee694
Paul Casavant <thepaulcasavant@gmail.com>
2021-02-15 09:51:06 -0800

    Got esp32 to compile

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,7 @@
 #include "WiFi.h"
 #include <WebSocketsClient.h>
 #include "password.h"
+#include <WebServer.h>
 #include <ESP32Ping.h>
  
 const int BUTTON_PIN = 14;
@@ -11,6 +12,33 @@ const char* PASSWORD =  WIFI_PASS;
 int buttonState = 0;
 
 WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  Serial.println("Websocket Event of type " + String(type));
+  
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n",  payload);
+      // send message to server when Connected
+      webSocket.sendTXT("Connected");
+      //webSocket.sendTXT("#E1");
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+      // send message to server
+      // webSocket.sendTXT("message here");
+      break;
+    // case WStype_BIN:
+    //   Serial.printf("[WSc] get binary length: %u\n", length);
+    //   hexdump(payload, length);
+    //   // send data to server
+    //   // webSocket.sendBIN(payload, length);
+    //   break;
+  }
+}
 
 void setup() {
  
@@ -36,36 +64,11 @@ void setup() {
 
     // Start websocket
     delay(500);
-    Serial.println("Starting WebSocket...");
-    webSocket.begin("10.55.25.121", 8080);
+    Serial.println("Starting WebSockets...");
+    webSocket.begin("10.55.25.121", 8080, "/", "arduino");
 
     // Event handler
     webSocket.onEvent(webSocketEvent);
-  }
-}
-
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("[WSc] Disconnected!\n");
-      break;
-    case WStype_CONNECTED:
-      Serial.printf("[WSc] Connected to url: %s\n",  payload);
-      // send message to server when Connected
-      webSocket.sendTXT("Connected");
-      //webSocket.sendTXT("#E1");
-      break;
-    case WStype_TEXT:
-      Serial.printf("[WSc] get text: %s\n", payload);
-      // send message to server
-      // webSocket.sendTXT("message here");
-      break;
-    // case WStype_BIN:
-    //   Serial.printf("[WSc] get binary length: %u\n", length);
-    //   hexdump(payload, length);
-    //   // send data to server
-    //   // webSocket.sendBIN(payload, length);
-    //   break;
   }
 }
  

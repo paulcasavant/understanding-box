1662fabb40bcc1b68f5d646213733568ee3db413
Paul Casavant <thepaulcasavant@gmail.com>
2021-02-25 15:40:26 -0800

    latest

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,35 @@
 // server.js
 
 const WebSocket = require('ws');
+const http = require("http");
+const express = require("express");
+const path = require("path");
 
-const wss = new WebSocket.Server({ port: 8080 });
+(function main() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const wss = new WebSocket.Server({ server: httpServer });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  app
+    .use(express.static(path.resolve("./public")))
+    .get("/clients", (req, res) => {
+      return res.status(200).json({
+        num_clients: wss.clients.size
+      });
+    });
+
+  // setup websocket handlers
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+      console.log('received: %s', message);
+
+      Array.from(wss.clients.values()).forEach((client) => {
+        client.send(message);
+      });
+    });
   });
 
-  ws.send('something');
-});
+  // Listen on this port
+  httpServer.listen(8080);
+
+})();

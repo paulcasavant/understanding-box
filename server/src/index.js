e3bfbb50581f27d0aeb3d286c3c3ab4d173b2d42
Paul Casavant <thepaulcasavant@gmail.com>
2021-03-01 21:35:07 -0800

    got on disconnect to work and created hashmap

--- server/src/index.js
+++ server/src/index.js
@@ -1,35 +1,42 @@
-import express from "express";
-import ws from "ws";
-import http from "http";
-import products from "./example.js";
+import express from 'express';
+import ws from 'ws';
+import http from 'http';
+import { v4 as uuidv4 } from 'uuid';
+//import products from "./example.js"; // TODO: Remove
 
 const port = 8080;
 const app = express();
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
 const wss = new ws.Server({ server: httpServer });
+let clientMap = new Map(); // Unique IDs for currently connected clients
 
-// Request, response callback function
-// app.get("/", (req, res) => {
-//   //res.send("Hello world");
-//   res.json(products);
-// })
+(function main() {
+  // Request, response callback function
+  // app.get("/", (req, res) => {
+  //   //res.send("Hello world");
+  //   res.json(products);
+  // })
 
-app.get("/clients", (req, res) => {
-  return res.status(200).json({
-    num_clients: wss.clients.size
-  });
-});
+  // app.get("/clients", (req, res) => {
+  //   return res.status(200).json({
+  //     num_clients: wss.clients.size
+  //   });
+  // });
 
-wss.on("connection", ws => {
-  console.log("New client connected");
+  wss.on('connection', client => {
+    console.log('[Server] Client connected (Total: %d)', wss.clients.size);
 
-  ws.on ("message", data => {
-    console.log('Recieved: %s', data);
-    ws.send(data.toUpperCase());
+    clientMap.set(uuidv4(), client);
+    console.log('Clients Size: %d', clientMap.size);
+
+    client.on ('message', data => {
+      console.log('[Server] Recieved: %s', data);
+      client.send(data.toUpperCase());
+    })
+    client.on('close', () => {
+      console.log('Client has disconnected');
+    })
   })
-  ws.on("close", () => {
-    console.log("Client has disconnected");
-  })
-})
 
-httpServer.listen(port);
+  httpServer.listen(port);
+})();

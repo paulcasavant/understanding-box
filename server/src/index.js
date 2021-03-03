36ddfb991d18d345fc9c3532f04a36aab5194615
Paul Casavant <thepaulcasavant@gmail.com>
2021-03-03 15:26:54 -0800

    implemented some major stuff

--- server/src/index.js
+++ server/src/index.js
@@ -16,6 +16,7 @@ const app = express();
 const httpServer = http.createServer(app);
 const wss = new ws.Server({ server: httpServer });
 let clientMap = new Map(); // UniqueIDs for currently connected clients
+let understandMap = new Map(); // Tracks whether a client understands or not
 var clientUUID; // Holds the UUID for the client
 
 (function main() {
@@ -37,13 +38,14 @@ var clientUUID; // Holds the UUID for the client
     clientUUID = uuidv4(); // Store UUID for this client
 
     clientMap.set(clientUUID, client);
+    understandMap.set(clientUUID, true);
     console.log('[Server] Client %d connected, UUID=%s', wss.clients.size, clientUUID);
 
     /* If the webpage has registered, notify of client connection */
     if (clientMap.has(WEB_UUID)) {
       clientMap.get(WEB_UUID).send(JSON.stringify({
         type: UPDATE,
-        message: CLIENT_CONNECT,
+        status: understandStatus(),
         size: wss.clients.size
       }))
     }
@@ -63,14 +65,38 @@ var clientUUID; // Holds the UUID for the client
       }
       /* Otherwise, message was received from a client */
       else {
-        clientMap.get(WEB_UUID).send(data);
+        clientMap.get(WEB_UUID).send(JSON.stringify({
+          type: UPDATE,
+          status: understandStatus(),
+          size: wss.clients.size
+        }))
 
         switch(data) {
-          /* Request a message response with the UUID for this client */
+          /* Client sets status to understand */
+          case 'understand':
+            understandMap.set(clientUUID, true);
+            break;
+
+          /* Client sets status to confused */
+          case 'confused':
+            understandMap.set(clientUUID, false);
+            break;
+
+          /* Client requests their UUID */
           case 'uuid':
             client.send("[Server] UUID: " + clientUUID);
             break;
-          
+
+          /* Client requests their current understanding status */
+          case 'status':
+            if (understandMap.get(clientUUID))
+            {
+              client.send("You understand.");
+            }
+            else
+            {
+              client.send("You are confused.");
+            }
           case 'custom':
             client.send("[Server] You called the custom message!");
         }
@@ -82,17 +108,37 @@ var clientUUID; // Holds the UUID for the client
       console.log('[Server] Client disconnected (Total: %d)', wss.clients.size);
       console.log('Clients Size: %d', clientMap.size);
 
-    /* If the webpage has registered, notify of client disconnection */
-    if (clientMap.has(WEB_UUID)) {
-      clientMap.get(WEB_UUID).send(JSON.stringify({
-        type: UPDATE,
-        message: CLIENT_CONNECT,
-        size: wss.clients.size
-      }))
-    }
-    
+      /* If the webpage has registered, notify of webpage client disconnection */
+      updateWebpage();
     })
   })
 
   httpServer.listen(PORT);
 })();
+
+function understandStatus()
+{
+  let count = 0;
+
+  for (let uuid of understandMap.keys())
+  {
+    if (clientMap.get(uuid))
+    {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+/* If the webpage has registered, send an update to the webpage */
+function updateWebpage(status)
+{
+  if (clientMap.has(WEB_UUID)) {
+    clientMap.get(WEB_UUID).send(JSON.stringify({
+      type: status,
+      status: understandStatus(),
+      size: wss.clients.size
+    })) 
+  }
+}
